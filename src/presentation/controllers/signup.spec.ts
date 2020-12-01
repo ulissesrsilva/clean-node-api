4bab250d8af81b2ce4b9bdf04e1fcbae490ea22a
Ulisses Silva <ulissesrdasilva@gmail.com>
2020-11-30 21:22:30 -0300

    test: ensure signup controller return an error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -13,6 +13,22 @@ describe('SignUp Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
-    // sut is system unit test
+  })
+})
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'nome',
+        // email: "teste@test.com.br",
+        password: 'pass',
+        passwordConfirmation: 'pass'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
