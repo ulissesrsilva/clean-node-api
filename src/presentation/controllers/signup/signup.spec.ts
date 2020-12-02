708eb7beac903cba4987759fc1b1bc120b0ae817
Ulisses Silva <ulissesrdasilva@gmail.com>
2020-12-02 17:53:49 -0300

    refactor: npm unit and integration test with error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -199,7 +199,7 @@ describe('SignUp Controller', () => {
         }
       }
       const httpResponse = await sut.handle(httpRequest)
-      expect(httpResponse.statusCode).toBe(500)
+      expect(httpResponse.statusCode).toBe(400)
       expect(httpResponse.body).toEqual(new ServerError())
   })
 
