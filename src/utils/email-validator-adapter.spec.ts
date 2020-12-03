a3ef60972dfac58e4acef24a70511d9bb84e1fb8
Ulisses Silva <ulissesrdasilva@gmail.com>
2020-12-03 10:04:34 -0300

    refactor: use a factory method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
    }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+    return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
     test('Should return false if validator returns false', () => {
-        const sut: EmailValidatorAdapter = new EmailValidatorAdapter()
+        const sut: EmailValidatorAdapter = makeSut()
         jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid: boolean = sut.isValid('invalid_email@mail.com')
         expect(isValid).toBe(false)
     })
 
     test('Should return true if validator returns true', () => {
-        const sut: EmailValidatorAdapter = new EmailValidatorAdapter()
+        const sut: EmailValidatorAdapter = makeSut()
         const isValid: boolean = sut.isValid('valid_email@mail.com')
         expect(isValid).toBe(true)
     })
 
     test('Should call validator with correct email', () => {
-        const sut: EmailValidatorAdapter = new EmailValidatorAdapter()
+        const sut: EmailValidatorAdapter = makeSut()
         const isEmailSpy = jest.spyOn(validator, 'isEmail')
         sut.isValid('any_email@mail.com')
         expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
